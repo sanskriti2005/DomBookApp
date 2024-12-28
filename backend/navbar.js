16ae68523ab28431b3f7dc0d90a3d7e329867e19
Sanskriti <sanskritisri27@gmail.com>
2024-12-28 14:14:39 +0530

    added portable navbar

---
+++ backend/navbar.js
@@ -0,0 +1,11 @@
+function loadNavbar() {
+    // catch nav element div
+    const nav = document.getElementById("nav")
+
+    nav.innerHTML = `<div id="navbar">
+            <div><a href="index.html">Login</a></div>
+            <div><a href="admin.html">Admin</a></div>
+            <div><a href="books.html">Books</a></div>
+        </div>`;
+}
+loadNavbar()
